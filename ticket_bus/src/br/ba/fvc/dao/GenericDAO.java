5b38340364fb4dc458ebec75510bd083d2e14ae7
iramaroliveira <iramar.oliveira@trc.sebraeba.com.br>
2023-05-25 09:53:25 -0300

    Feito ajuste na mensagem para apagar registros atrelado a outros

--- ticket_bus/src/br/ba/fvc/dao/GenericDAO.java
+++ ticket_bus/src/br/ba/fvc/dao/GenericDAO.java
@@ -110,7 +110,7 @@ public class GenericDAO {
 		} catch (SQLException e) {
 			if (e.getErrorCode() == 1451) {
 				JOptionPane.showMessageDialog(null, "(" + this.table.toUpperCase()
-						+ ") não pode ser excluida, existe registros esta atrelado a outros registros.");
+						+ ") não pode ser excluido(a), este registro esta atrelado a outros registros.");
 				return;
 			}
 			JOptionPane.showMessageDialog(null, e.getMessage());
